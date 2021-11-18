01f015046f444b6e0721c7d1e695c9f0f8e5874e
aviel780 <aviel780@gmail.com>
2021-11-19 00:08:52 +0200

    fifth commit

--- Offline.py
+++ Offline.py
@@ -1,58 +1,63 @@
-import Building
 import math
+from main import main
+
 from Elevator import Elevator
 from Building import Building
 from Calls import Calls
 
 
 class Offline:
-    def __init__(self, building: Building, cl_list: list):
+    def __init__(self, building: Building, cl_list: list,csv_name):
         self.building = building
         self.cl_list = cl_list
+        self.csv_name=csv_name
 
-    def allocated(self, call, indexs: list):
-        index = 0  # default, num of elevator
-        i = 0  # represents the index in the loop
-        min_time = 100000000
-        if self.contains_level(self.building.elevators):
-            # search for the elevator with the shortest arrive time and in level mode
-            index = self.level_best_time(self, call)
-        else:
-            for elev in self.building.elevators:  # if there isn't an elevator in level mode
-                if elev.state == -2:  # if error, pass
-                    i += 1
-                    continue
-                if elev.state != 0:  # not in level mode
-                    if elev.type is not call.type:  # if the elevator is going up and the call is down-> continue, vice versa
+    def allocated(self, call):
+        call_list= main.load_csv_file(self.csv_name)
+        for i in call_list:
+
+            index = 0  # default, num of elevator
+            i = 0  # represents the index in the loop
+            min_time = 100000000
+            if self.contains_level(self.building.elevators):
+                # search for the elevator with the shortest arrive time and in level mode
+                index = self.level_best_time(self, call)
+            else:
+                for elev in self.building.elevators:  # if there isn't an elevator in level mode
+                    if elev.state == -2:  # if error, pass
                         i += 1
                         continue
-                    if elev.type == 1 and call.type == 1:  # UP
-                        if elev.max_floor < call.dest:
+                    if elev.state != 0:  # not in level mode
+                        if elev.type is not call.type:  # if the elevator is going up and the call is down-> continue, vice versa
                             i += 1
                             continue
-                        if elev.min_floor > call.src:
-                            i += 1
-                            continue
-                    if elev.type == -1 and call.type == -1:  # DOWN
-                        if elev.max_floor < call.src:
-                            i += 1
-                            continue
-                        if elev.min_floor > call.dest:
-                            i += 1
-                            continue
-                    num_of_calls = len(elev.calls_list)
-                    sum_distance = self.total_distance_floors(elev)
-                    total_open_close = math.ceil(num_of_calls * (elev.open_time + elev.close_time))
-                    total_start_stop = math.ceil(num_of_calls * (elev.start_time + elev.stop_time))
-                    ride_time = math.ceil(sum_distance * elev.time_per_floor)
-                    temp_time = total_start_stop + total_open_close + ride_time
-                    if min_time > temp_time:
-                        min_time = temp_time
-                        index = i
-                i += 1
-        elev.calls_list.add(call)
-        indexs.append(index)
-        return index
+                        if elev.type == 1 and call.type == 1:  # UP
+                            if elev.max_floor < call.dest:
+                                i += 1
+                                continue
+                            if elev.min_floor > call.src:
+                                i += 1
+                                continue
+                        if elev.type == -1 and call.type == -1:  # DOWN
+                            if elev.max_floor < call.src:
+                                i += 1
+                                continue
+                            if elev.min_floor > call.dest:
+                                i += 1
+                                continue
+                        num_of_calls = len(elev.calls_list)
+                        sum_distance = self.total_distance_floors(elev)
+                        total_open_close = math.ceil(num_of_calls * (elev.open_time + elev.close_time))
+                        total_start_stop = math.ceil(num_of_calls * (elev.start_time + elev.stop_time))
+                        ride_time = math.ceil(sum_distance * elev.time_per_floor)
+                        temp_time = total_start_stop + total_open_close + ride_time
+                        if min_time > temp_time:
+                            min_time = temp_time
+                            index = i
+                    i += 1
+            elev.calls_list.add(call)
+            indexs.append(index)
+            return index
 
     def total_distance_floors(elev: Elevator) -> int:
         total = 0
--- main.py
+++ main.py
@@ -6,38 +6,39 @@ from Building import Building
 from Offline import Offline
 
 
-def main(file_name, csv_name):
-    def load_json_file(file_name):
-        try:
-            with open(file_name, "r") as file:
-                config = json.load(file)
-                build = Building(config["_minFloor"], config["_maxFloor"], config["_elevators"])
-                print(3)
-                return build
-        except IOError as e:
-            print(e)
 
-    def load_csv_file(csv_name):
-        try:
-            with open(csv_name) as csv_file:
-                calls_reader = csv.reader(csv_file)
-                elv_list = list()
-                for line in calls_reader:
-                    call = Calls(line[1],line[2], line[3])
-                    Offline.allocated(call, elv_list)
+def load_json_file(file_name):
+    try:
+        with open(file_name, "r") as file:
+            config = json.load(file)
+            build = Building(config["_minFloor"], config["_maxFloor"], config["_elevators"])
+            return build
+    except IOError as e:
+        print(e)
 
-            extract_back_to_csv(elv_list, csv_name)
-        except IOError as e:
-            print(e)
+def load_csv_file(csv_name):
+    try:
+        with open(csv_name) as csv_file:
+            calls_reader = csv.reader(csv_file)
+            elv_list = list()
+            list_calls=list()
+            for line in calls_reader:
+                call = Calls(line[1],line[2], line[3])
+                list_calls.append(call)
+        return list_calls
+                  #  Offline.allocated(call, elv_list)
+            #extract_back_to_csv(elv_list, csv_name)
+    except IOError as e:
+        print(e)
 
-    def extract_back_to_csv(calls_list, name):
-        f = open(name, "w")  # name of assigned calls csv
-        for y in range(0, len(calls_list) - 1):
-            str_c = str(calls_list[y])
-            f.write(str_c + "\n")
-        str_c = str(calls_list[len(calls_list) - 1])  # last line without \n
-        f.write(str_c)
-        f.close()
-
+def extract_back_to_csv(calls_list, name):
+    f = open(name, "w")  # name of assigned calls csv
+    for y in range(0, len(calls_list) - 1):
+        str_c = str(calls_list[y])
+        f.write(str_c + "\n")
+    str_c = str(calls_list[len(calls_list) - 1])  # last line without \n
+    f.write(str_c)
+    f.close()
+if __name__ == "__main__":
     load_json_file(file_name)
     load_csv_file(csv_name)
