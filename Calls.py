60e484f49f78823c81892b072d19faa95b51c6a6
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2021-11-19 13:49:08 +0200

    Update Calls.py

--- Calls.py
+++ Calls.py
@@ -1,22 +1,41 @@
+import Elevator
+
 class Calls:
+    INIT = 0
+    GOING2SRC = 1
+    GOIND2DEST = 2
+    DONE = 3
+    UP = 1
+    DOWN = -1
 
-    def __init__(self, timing, src, dest):
-        self.timing = float(timing)
-        self.src = int(src)
-        self.dest = int(dest)
-        self.num_of_elev = 0  # default
-        if (self.dest - self.src) > 0:
-            self.type = 1  # UP
-        if (self.dest - self.src) < 0:
-            self.type = -1  # DOWN
+    def __init__(self, _callTime, _src, _dest):
+        self._callTime = _callTime
+        self.src = _src
+        self.dest = _dest
+        self._assignedEle = -1
 
-    def assign_elevator(self, num):
-        self.num_of_elev = num
 
-    # def allocatedTo(self):
-    #   """This methods return the index of the Elevator in the building to which this call
-    #   was assigned to, if not yet Assigned --> return -1"""
-    # if self._assignedEle == None:
-    #    return -1
-    # return self._assignedEle
-#       #  return self._assignedEle
+    def get_call_time(self):
+        return self._callTime
+    def getSrc(self):
+        return (int)(self.src)
+
+    def getDest(self):
+        return (int)(self.dest)
+
+    def getType(self):
+        if self.src > self.dest:
+            return self.DOWN
+        return self.UP
+
+    def setElevator(self, index):
+        self._assignedEle = index
+
+    def allocatedTo(self):
+        if self._assignedEle == None:
+            return -1
+        return self._assignedEle
+
+    def __str__(self):
+        return f"the time of call: {self._callTime} \t source floor: {self.src} \t destination floor: {self.dest} \n\t" \
+               f"the elevator of the call: {self._assignedEle}"
