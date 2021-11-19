b554902ef9adda6106c4e32ba99d509d52e792bc
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2021-11-19 13:48:09 +0200

    Update Building.py

--- Building.py
+++ Building.py
@@ -1,9 +1,16 @@
-import Elevator
+from Elevator import elevator
+import pandas as pd
+import json
 
 
 class Building:
+    def __init__(self, _minFloor, _maxFloor, _elevators):
+        self._maxFloor = _maxFloor
+        self._minFloor = _minFloor
+        self._elevators_list = []
+        for item in _elevators:
+            self._elevators_list.append(elevator(_id=item["_id"], _speed=item["_speed"], _minFloor=item["_minFloor"],
+                                                 _maxFloor=item["_maxFloor"], _closeTime=item["_closeTime"],
+                                                 _openTime=item["_openTime"], _startTime=item["_startTime"],
+                                                 _stopTime=item["_stopTime"]))
 
-    def __init__(self, min_floor: int, max_floor: int, elevators: list):
-        self.min_floor_building = min_floor
-        self.max_floor_building = max_floor
-        self.elevators = elevators
