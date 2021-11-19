6b5de2d232e0f90e2a80c1ab00853301e397c5dc
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2021-11-19 13:49:26 +0200

    Update Elevator.py

--- Elevator.py
+++ Elevator.py
@@ -1,28 +1,64 @@
-class Elevator:
+from Calls import Calls
 
-    def __init__(self, ID, min_floor, max_floor, open_time, close_time, start_time, stop_time, speed):
-        self.state = 0
-        self.ID = ID
-        self.min_floor = min_floor
-        self.max_floor = max_floor
-        self.open_time = open_time
-        self.close_time = close_time
-        self.start_time = start_time
-        self.stop_time = stop_time
-        self.speed = speed
+class elevator():
+    UP = 1
+    LEVEL = 0
+    DOWN = -1
+    ERROR = -2
+
+    def __init__(self, _id, _speed, _minFloor, _maxFloor, _closeTime, _openTime,_startTime, _stopTime):
+        self.id = _id
+        self.speed = _speed
+        self.minFloor = _minFloor
+        self.maxFloor = _maxFloor
         self.time_per_floor = 1 / self.speed
-        self.calls_list = []
+        self.close_time = _closeTime
+        self.open_time = _openTime
+        self.start_time = _startTime
+        self.stop_time = _stopTime
+        self.call_list = []
 
-    # self.calls_list.append(AllocatedCall.first_call(self))
-    # state: leve = 0, up = 1, down = -1, error = -2
 
-    def insert_call(self, call):
-        self.calls_list += [call]
+    def UpdateListOfCalls(self, call):
+        print(self.call_list)
+        self.call_list.append(call)
 
-    def current_state(self):
-        if len(self.calls_list) == 0:
-            self.state = 0
-        if self.calls_list[0].dest - self.calls_list[0].src > 0:
-            self.state = 1
-        if self.calls_list[0].dest - self.calls_list[0].sec < 0:
-            self.state = -1
+    def getID(self):
+        return (int)(self.id)
+
+    def getSpeed(self):
+        return (float)(self.speed)
+
+    def getMinFloor(self):
+        return (int)(self.minFloor)
+
+    def getMaxFloor(self):
+        return (int)(self.maxFloor)
+
+    def gettimeForOpen(self):
+        return (float)(self.openTime)
+
+    def gettimeForClose(self):
+        return (float)(self.closeTime)
+
+    def getState(self):
+        if len(self.call_list) == 0:
+            return self.LEVEL
+        if self.call_list[len(self.call_list) - 1].getType() is Calls.UP:
+            return self.UP
+        elif self.call_list[len(self.call_list) - 1].getType() is Calls.DOWN:
+            return self.DOWN
+        return self.LEVEL
+
+
+    def getStartTime(self):
+        return (float)(self.startTime)
+
+    def getStopTime(self):
+        return (float)(self.stopTime)
+
+    def __str__(self):
+        return f"elevator number {self.id}: \n\t speed = {self.speed} \n\t minFloor = {self.minFloor} \n\t" \
+               f"maxFloor = {self.maxFloor} \n\t close time = {self.open_time} \n\t" \
+               f"open time = {self.open_time} \n\t start time = {self.start_time} \n\t" \
+               f"stop time = {self.stop_time}"
