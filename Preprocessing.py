26ce855241233ce0234dd90ee170a244b4e13681
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2021-11-19 13:51:10 +0200

    Update and rename main.py to Preprocessing.py

---
+++ Preprocessing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def saveToCsv(file_name, callsArray):
+    df = pd.DataFrame(callsArray)
+    df.to_csv(str(file_name))
+
+
+def readCsv(file_name):
+    df = pd.read_csv(file_name)
+    rows = []
+    for index, row in df.iterrows():
+        rows.append(list(row))
+    print(rows)
+    return rows
+
+
+def ReadJson(file_name):
+    building_df = pd.read_json(file_name)
+    return building_df
