52be7636909f2f43e7658ebb7356423d6a35fcec
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2021-11-20 17:59:58 +0200

    Update Building.py

--- Building.py
+++ Building.py
@@ -9,10 +9,8 @@ class Building:
         self._minFloor = _minFloor
         self._elevators_list = []
         for item in _elevators:
-            self._elevators_list.append(Elevator(_id=item["_id"], _speed=item["_speed"], _minFloor=item["_minFloor"],
-                                                 _maxFloor=item["_maxFloor"], _closeTime=item["_closeTime"],
-                                                 _openTime=item["_openTime"], _startTime=item["_startTime"],
-                                                 _stopTime=item["_stopTime"]))
+            self._elevators_list.append(Elevator(_id=item["_id"], _speed=item["_speed"], _minFloor=item["_minFloor"], _maxFloor=item["_maxFloor"], _closeTime=item["_closeTime"],
+                                                 _openTime=item["_openTime"], _startTime=item["_startTime"],_stopTime=item["_stopTime"]))
 
     @property
     def elevators_list(self):
